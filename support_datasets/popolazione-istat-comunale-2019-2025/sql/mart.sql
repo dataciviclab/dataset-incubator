4d3c0ff0d37d3ce9bef902214a1b022260aa794d
Zio Gabber <78922322+Gabrymi93@users.noreply.github.com>
2026-05-25 21:12:29 +0100

    feat(popolazione): mart hierarchy fascia eta + exclude_metrics + fix doppio conteggio totali
    Clean SQL: aggiunta colonna fascia_eta (6 fasce), esclusa riga eta=999
    Mart SQL: popolazione_by_comune passa da SELECT eta=999 a SUM GROUP BY comune
    Hierarchy: singolo livello fascia_eta (esclude h_eta e h_comune ridondanti)

--- support_datasets/popolazione-istat-comunale-2019-2025/sql/mart.sql
+++ support_datasets/popolazione-istat-comunale-2019-2025/sql/mart.sql
@@ -2,10 +2,10 @@ select
   {year} as anno_riferimento,
   codice_comune,
   comune,
-  popolazione_residente,
-  totale_maschi as popolazione_maschile,
-  totale_femmine as popolazione_femminile
+  sum(popolazione_residente) as popolazione_residente,
+  sum(totale_maschi) as popolazione_maschile,
+  sum(totale_femmine) as popolazione_femminile
 from clean_input
 where codice_comune is not null
   and comune is not null
-  and eta = 999
+group by codice_comune, comune
