48b54db9c2cb62b44a1e734ffd2291bb03cfd0bb
Zio Gabber <78922322+Gabrymi93@users.noreply.github.com>
2026-07-13 12:42:56 +0100

    fix: ripristina candidate mur-immatricolati con file corretti e pipeline funzionante

--- candidates/mur-immatricolati/sql/clean.sql
+++ candidates/mur-immatricolati/sql/clean.sql
@@ -9,3 +9,4 @@ WHERE
     TRY_CAST(SUBSTR(AnnoA, 1, 4) AS INTEGER) IS NOT NULL
     AND TRY_CAST(Immatricolati AS INTEGER) IS NOT NULL
     AND Immatricolati >= 0
+-- DROP _id: CKAN internal row id, non utile
